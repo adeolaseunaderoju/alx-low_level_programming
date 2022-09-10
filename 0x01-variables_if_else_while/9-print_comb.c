e69f13ad2a0d27f3ec68acbd5ef8354322a92bd8
adeolaseunaderoju <adeolaseunaderoju@gmail.com>
2022-09-09 21:38:44 -0700

    Printing

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,22 +7,19 @@
  */
 int main(void)
 {
-	 int c;
+int c;
 
-	 for (c = '0'; c <= '9'; c++)
-	{
-		 putchar(c);
-	
-		 if (c != '9')
-		{
-			putchar(',');
-			  putchar(' ');
-		}
-	 }
-
-	 putchar('\n');
-
-	 return (0);
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);	
+if (c != '9')
+{	
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
 	
 

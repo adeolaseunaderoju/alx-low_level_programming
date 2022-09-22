805fea8a579617667abfd732c476febc1dc575fb
adeolaseunaderoju <adeolaseunaderoju@gmail.com>
2022-09-22 09:46:03 -0700

    a function that compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- *  _strcmp - Function which compare two strings and
- *  @s1: first string
- *  @s2:second string
- *  Return:
- * 	 returns zero if s1 == s2
- * 	 returns negative number if s1 < s2
- * 	 returns positive number if s1 > s2
- * /
- 
- int _strcmp(char *s1, char *s2)
- {
- 	int i = 0, diff = 0;
+ * _strcmp - Function which compare two strings and
+ * @s1: first string
+ * @s2:second string
+ * Return:
+ * 	returns zero if s1 == s2
+ * 	returns negative number if s1 < s2
+ * 	returns positive number if s1 > s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, diff = 0;
 
 	while (1)
 	{
@@ -35,6 +35,7 @@
 		}
 		else
 			i++;
-		}
-		return (diff);
+
+	}
+	return (diff);
 }

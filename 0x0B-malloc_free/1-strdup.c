8cb7e34ede5c6530466400346589ac7f7123d25e
adeolaseunaderoju <adeolaseunaderoju@gmail.com>
2022-10-13 14:23:11 -0700

    Task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,30 +1,44 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * _strdup - allocate a string.
- * @str: string.
- * Return: pointer to string if success.
+ * _strlen - returns the length of the string
+ * @s: input string to count
+ * Description: returns the length of a given string
+ * Return: length of string as int
+ */
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++);
+	return (i + 1);
+}
+
+/**
+ * _strdup - duplicate string
+ * @str: source string
+ * Description: duplicate string
+ * Return: pointer to string, NULL if fail
  */
 
 char *_strdup(char *str)
 {
-	int i;
-	char *str1;
+	char *dest;
+	unsigned int i;
 
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; str[i]; i++)
-	;
-	i++;
-	str1 = malloc(sizeof(char) * i);
+	dest = (char *)malloc((_strlen(str)) * sizeof(char));
 
-	if (str1 == NULL)
+	if (dest == NULL)
 		return (NULL);
 
 	for (i = 0; str[i] != '\0'; i++)
-		str1[i] = str[i];
-	str1[i] = '\0';
-	return (str1);
+		dest[i] = str[i];
+	dest[i] = str[i];
+
+	return (dest);
 }

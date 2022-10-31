24c118611c686b664cf1391551c1a88f26345a13
adeolaseunaderoju <adeolaseunaderoju@gmail.com>
2022-10-31 14:37:42 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,6 +8,7 @@
  * @argv: arguments vector.
  * Return: no return.
  */
+
 void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
@@ -15,7 +16,7 @@ void error_file(int file_from, int file_to, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (file_to == -1
+	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
@@ -28,6 +29,7 @@ void error_file(int file_from, int file_to, char *argv[])
  * @argv: arguments vector.
  * Return: Always 0.
  */
+
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
@@ -39,11 +41,9 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
-
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
-
 	nchars = 1024;
 	while (nchars == 1024)
 	{
@@ -54,14 +54,12 @@ int main(int argc, char *argv[])
 		if (nwr == -1)
 			error_file(0, -1, argv);
 	}
-
 	err_close = close(file_from);
 	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-
 	err_close = close(file_to);
 	if (err_close == -1)
 	{
@@ -70,5 +68,3 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 }
-
-
